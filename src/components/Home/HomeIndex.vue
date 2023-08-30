a1629f9e1d4f1fb98d470fb987dbcc581ebe26ac
RyanL <a11160725@gmail.com>
2023-08-30 22:27:18 +0800

    8/30 update

---
+++ src/components/Home/HomeIndex.vue
@@ -0,0 +1,4 @@
+<script>
+export default {};
+</script>
+<template src="./template.html"></template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../components/Home/HomeIndex.vue";
 
 const routes = [
   {
