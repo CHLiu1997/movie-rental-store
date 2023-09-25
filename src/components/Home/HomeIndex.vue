e0e4292b1f254f91dc5b5abbd00c4c6ab92977db
RyanL <a11160725@gmail.com>
2023-09-25 22:48:09 +0800

    20230925 update

--- src/components/Home/HomeIndex.vue
+++ src/components/Home/HomeIndex.vue
@@ -1,38 +1,5 @@
 <script>
-export default {
-  data() {
-    return {
-      slideIndex: 1,
-      methods: {
-        prevSlide() {
-          this.showSlides((this.slideIndex -= 1));
-        },
-        nextSlide() {
-          this.showSlides((this.slideIndex += 1));
-        },
-        showSlides(n) {
-          let slides = document.getElementsByClassName("mySlides");
-          let dots = document.getElementsByClassName("dot");
-          if (n > slides.length) {
-            this.slideIndex = 1;
-          }
-          if (n < slides.length) {
-            this.slideIndex = slides.length;
-          }
-          for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-          }
-          for (let i = 0; i < slides.length; i++) {
-            // dots[i].className = dots[i].className.replace(" active", "");
-            dots[i].classList.remove("active");
-          }
-          slides[this.slideIndex - 1].style.display = "block";
-          dots[this.slideIndex - 1].classList.add("active");
-        },
-      },
-    };
-  },
-};
+export default {};
 </script>
 <template src="./template.html"></template>
 <style src="./style.css"></style>
