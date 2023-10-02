b20e73d590b512404fcff48b0dfc8c0e7de36381
RyanL <a11160725@gmail.com>
2023-10-02 16:47:42 +0800

    20231002 update

--- src/components/Home/HomeIndex.vue
+++ src/components/Home/HomeIndex.vue
@@ -1,5 +1,13 @@
 <script>
-export default {};
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import "@splidejs/vue-splide/css/customize";
+
+export default {
+  components: {
+    Splide,
+    SplideSlide,
+  },
+};
 </script>
 <template src="./template.html"></template>
 <style src="./style.css"></style>
