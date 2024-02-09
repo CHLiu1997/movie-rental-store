58f5dd8a4730f594263c135016d4f6c4730f4280
RyanL <a11160725@gmail.com>
2024-02-09 18:42:03 +0800

    20240209

--- src/pages/Subscribe/SubscribePage.vue
+++ src/pages/Subscribe/SubscribePage.vue
@@ -1,5 +1,23 @@
 <script>
-export default {};
+const setup = () => {
+  function subscribeHandler() {
+    let discount = prompt("請輸入優惠碼");
+    if (discount == null) {
+      alert("訂閱失敗");
+    } else if (discount == "") {
+      alert("訂閱成功");
+    } else if (discount == "justwatch") {
+      alert("優惠碼輸入成功");
+      alert("訂閱成功");
+    } else {
+      alert("請輸入正確的優惠碼");
+    }
+  }
+  return {
+    subscribeHandler,
+  };
+};
+export default { setup };
 </script>
 <template src="./template.html" />
 <style src="./style.css" />
